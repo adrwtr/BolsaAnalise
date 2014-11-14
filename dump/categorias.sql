18c09fcfe63dd9132d2fdae689ffbebb2164c1ad
Adriano Waltrick <akuma.majunia@gmail.com>
2014-11-14 10:56:15 -0200

    Fontes gerais

--- dump/categorias.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `categorias` (`cd_categoria`, `ds_categoria`) VALUES (1, 'Política');
-INSERT INTO `categorias` (`cd_categoria`, `ds_categoria`) VALUES (2, 'Negócios');
-INSERT INTO `categorias` (`cd_categoria`, `ds_categoria`) VALUES (3, 'Fatos');
--- dump/papeis.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `papeis` (`cd_papel`, `ds_papel`, `ds_chave`) VALUES (1, 'Petrobras 4', 'PETR4');
-INSERT INTO `papeis` (`cd_papel`, `ds_papel`, `ds_chave`) VALUES (2, 'Eztec', 'EZTC3');
-INSERT INTO `papeis` (`cd_papel`, `ds_papel`, `ds_chave`) VALUES (3, 'Vale 5', 'VALE5');
-INSERT INTO `papeis` (`cd_papel`, `ds_papel`, `ds_chave`) VALUES (4, 'Banco Brasil 3', 'BBAS3');
-INSERT INTO `papeis` (`cd_papel`, `ds_papel`, `ds_chave`) VALUES (6, 'Banco Paraná', 'PRBC4');
-INSERT INTO `papeis` (`cd_papel`, `ds_papel`, `ds_chave`) VALUES (7, 'ETERNIT', 'ETER3');
